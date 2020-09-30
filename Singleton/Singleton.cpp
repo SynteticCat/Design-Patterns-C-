70e0a6aa9e57a297319656ef5e2aa0576c2be7d8
Илья <sviridov9199@gmail.com>
2020-09-30 21:45:21 +0300

    add Singleton

---
+++ Singleton/Singleton.cpp
@@ -0,0 +1,31 @@
+﻿# include <iostream>
+# include <memory>
+
+using namespace std;
+
+class Product
+{
+public:
+	static shared_ptr<Product> instance()
+	{
+		static shared_ptr<Product> myInstance(new Product());
+
+		return myInstance;
+	}
+	~Product() { cout << "Destructor;" << endl; }
+
+	void f() { cout << "Method f;" << endl; }
+
+	Product(const Product&) = delete;
+	Product& operator=(const Product&) = delete;
+
+private:
+	Product() { cout << "Default constructor;" << endl; }
+};
+
+int main()
+{
+	shared_ptr<Product> ptr(Product::instance());
+
+	ptr->f();
+}
