4a811c2360bcdfdf93e58e0f452eddb3d7357265
Илья <sviridov9199@gmail.com>
2020-10-01 02:16:58 +0300

    add Decorator

---
+++ Decorator/Decorator.cpp
@@ -0,0 +1,69 @@
+﻿# include <iostream>
+# include <memory>
+
+using namespace std;
+
+// любой компонент
+class Component
+{
+public:
+	virtual ~Component() = 0;
+
+	virtual void operation() = 0;
+};
+
+Component::~Component() {}
+
+class ConComponent : public Component
+{
+public:
+	virtual void operation() override { cout << "ConComponent; "; }
+};
+
+class Decorator : public Component
+{
+protected:
+	// содержит ссылку на компонент
+	shared_ptr<Component> component;
+
+public:
+	Decorator(shared_ptr<Component> comp) : component(comp) {}
+};
+
+class ConDecorator : public Decorator
+{
+public:
+	using Decorator::Decorator;
+
+	// подменяет метод operation
+	virtual void operation() override;
+};
+
+#pragma region Method
+// сам метод содержит вызов operation и дополнительный функционал
+void ConDecorator::operation()
+{
+	if (component)
+	{
+		component->operation();
+
+		cout << "ConDecorator ";
+	}
+
+}
+
+#pragma endregion
+
+int main()
+{
+	shared_ptr<Component> component(new ConComponent());
+	shared_ptr<Component> decorator1(new ConDecorator(component));
+
+	decorator1->operation();
+	cout << ";" << endl;
+
+	shared_ptr<Component> decorator2(new ConDecorator(decorator1));
+
+	decorator2->operation();
+	cout << ";" << endl;
+}
