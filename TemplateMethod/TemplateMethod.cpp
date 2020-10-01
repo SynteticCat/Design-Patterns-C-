5be288e9f3bb5ac06c5e351be587b14b4d417817
Илья <sviridov9199@gmail.com>
2020-10-01 07:36:30 +0300

    add Template Method

---
+++ TemplateMethod/TemplateMethod.cpp
@@ -0,0 +1,40 @@
+﻿# include <iostream>
+
+using namespace std;
+
+class AbstractClass
+{
+public:
+	void templateMethod()
+	{
+		primitiveOperation();
+		concreteOperation();
+		hook();
+	}
+
+protected:
+	virtual void primitiveOperation() = 0;
+	void concreteOperation() { cout << "concreteOperation;" << endl; }
+	virtual void hook() { cout << "hook Base;" << endl; }
+};
+
+class ConClassA : public AbstractClass
+{
+protected:
+	virtual void primitiveOperation() override { cout << "primitiveOperation A;" << endl; }
+};
+
+class ConClassB : public AbstractClass
+{
+protected:
+	virtual void primitiveOperation() override { cout << "primitiveOperation B;" << endl; }
+	void hook() { cout << "hook B;" << endl; }
+};
+
+int main()
+{
+	ConClassA ca;
+	ConClassB cb;
+	ca.templateMethod();
+	cb.templateMethod();
+}
